5c65b08aeb1fc713b94b75f4304aa45e945675c3
Duriel7 <duriel@live.fr>
2025-01-13 14:02:37 +0100

    fix: deleted unwanted training files + organized source folder

--- Exo1.py
+++
@@ -1,7 +0,0 @@
-tva = 0.2;
-
-commande = int(input("Quel prix ?\n"));
-
-prix = commande * (1 + tva);
-
-print(prix);
--- Exo2.py
+++
@@ -1,9 +0,0 @@
-import math
-
-ray = int(input("Quel rayon ?\n"));
-
-h = int(input("Quelle hauteur ?\n"));
-
-volume = (math.pi * (ray ** 2) * h)/3;
-
-print("Votre cône a un volume de", volume);
