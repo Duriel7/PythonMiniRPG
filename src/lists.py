ad494c0acaaa92eecdb7e61a6a63260e624b9f2b
Duriel7 <duriel@live.fr>
2025-02-12 13:36:25 +0100

    WIP: prepared lists file first step

---
+++ src/lists.py
@@ -0,0 +1,55 @@
+import random, math, sys
+from database_feed import *
+
+#This file will contain all items lists that will be needed for the game
+
+#General lists
+
+    #Characters
+
+        #NPC human
+
+        #Monsters
+
+        #Pets
+
+    #Inhabited places hardcoded
+
+        #Villages
+
+        #Main cities
+
+    #Dungeons
+
+        #Subterranean
+
+        #Overworld
+
+        #Air placed
+
+        #Other dimensions
+
+    #Items
+
+        #General items
+itemList = []
+
+        #General categories
+aidList = []
+weaponList = []
+armorList = []
+
+        #Precise categories
+
+            #Weapons
+swordList = []
+axeList = []
+spearList = []
+polearmList = []
+bowList = []
+crossbowList = []
+
+
+            #Armors
+
+            #Potions
